ceefb8731e1624a70c5aae885b025761c38371d9
Yoohan Ko <yoohanko98@gmail.com>
2020-02-19 14:52:35 -0500

    Create test file for US03

---
+++ gedcomreader/TestUS03.java
@@ -0,0 +1,40 @@
+package gedcomreader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author yoohanko98
+ */
+
+public class TestUS03 {
+    public TestUS03 {
+    }
+
+    @Test
+    public void testbirthBeforeDeath () throws IOException, FileNotFoundException, ParseException {
+        system.out.println("Testing US03");
+        GED testinstance = new GED();
+        testinstance.traversal();
+        testinstance.errorPrint();
+
+        assertTrue(true);
+
+        assertNotNull(testinstance.individuals);
+        assertNotNull(testinstance.families);
+
+        if (instance.errors.size() <= 0) {
+            fail("the exception number is wrong");
+        }
+        else {
+            System.out.println("the number of errors is correct");
+        }
+
+
+
+    }
+
+
+}

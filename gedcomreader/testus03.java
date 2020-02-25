8f15cd409e1dcc2b8f8f86f2936a4d96b05d4655
Yoohan Ko <yoohanko98@gmail.com>
2020-02-25 15:12:50 -0500

    Fixed empty test file

--- gedcomreader/testus03.java
+++ gedcomreader/testus03.java
@@ -1,4 +1,49 @@
 package gedcomreader;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author yoohanko98
+ */
+
 public class testus03 {
+    public testus03() {
+    }
+
+    @Test
+    public void testbirthBeforeDeath () throws IOException, FileNotFoundException, ParseException {
+        System.out.println("Testing US03");
+        GED instance = new GED();
+
+        instance.traversal();
+        instance.errorsPrint();
+
+        // Test #1
+        assertNotNull(instance.individuals); // checking whether individuals exists within the GED instance
+
+        // Test #2
+        assertNotNull(instance.families); // checking whether families exists within the GED instance
+
+        // Test #3
+        assertNotNull(instance.errors); // checking whether place to put errors exists within the GED instance
+
+        // Test #4
+        assertTrue(true);
+
+        // Test #5
+        if (!instance.errors.contains("Error US03: Birthday of Terence /Holland/(I10) occurs after death date.")) {
+            fail("the first exception information is wrong");
+        }
+        else {
+            System.out.println("set of errors contains the first error");
+        }
+
+        System.out.println("GED test for US03 passed");
+    }
+
+
 }

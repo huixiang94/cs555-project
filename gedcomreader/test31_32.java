f8981f329e824722ec91f858d8f36254ae0a32c7
Yoohan Ko <yoohanko98@gmail.com>
2020-04-22 19:43:33 -0400

    Completed & Tested US32

--- gedcomreader/test31_32.java
+++ gedcomreader/test31_32.java
@@ -36,6 +36,22 @@ public class test31_32 {
 
         assertTrue(instance.livingSingle.contains("Bloom /Jams/(I13)"));
         assertTrue(instance.livingSingle.contains("Jake /Smith/(I18)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I19)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I20)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I21)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I22)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I23)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I24)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I25)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I26)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I27)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I28)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I29)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I30)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I31)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I32)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I33)"));
+        assertTrue(instance.livingSingle.contains("Jake /Smith/(I34)"));
 
         System.out.println("test of US31 passed");
     }
@@ -59,9 +75,8 @@ public class test31_32 {
             System.out.println("the number of errors is correct");
         }
 
-        assertTrue(instance.errors.contains("There are two same birthdays:  "));
-        assertTrue(instance.errors.contains("There are two same birthdays:  "));
-        assertTrue(instance.errors.contains("There are two same birthdays:  "));
+        assertTrue(instance.errors.contains("There are two same birthdays: 09-16-1972"));
+        assertTrue(instance.errors.contains("There are two same birthdays: 10-15-1948"));
 
         System.out.println("test of US32 passed");
     }
